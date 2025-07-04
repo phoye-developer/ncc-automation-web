c0fcd8d4f12e7ddda71140e41f181b654e654496
Patrick Hoye <phoye.developer@gmail.com>
2025-07-04 13:36:24 -0700

    Add queueId, promptId check to script processing

--- admin/sign-in.php
+++ admin/sign-in.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <!--
 Author: Nextiva, Inc.
-Product Name: Nextiva Automation Tool Version: 1.1.0
+Product Name: Nextiva Automation Tool Version: 1.1.1
 Website: https://www.nextiva.com
 Contact: phoye@nextiva.com
 -->
